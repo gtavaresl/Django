16312178fdd68d2d88c2286ef2e3d989c1391872
gtavaresl <gabrielltavsb@gmail.com>
2019-05-22 12:37:36 -0300

    New field, link from Youtube.

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from django.utils import timezone
 
-
 class Post(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=50)
+    band = models.CharField(max_length=50)
+    link = models.URLField(max_length=100, blank=True, null = True)
     text = models.TextField()
     created_date = models.DateTimeField(
             default=timezone.now)
